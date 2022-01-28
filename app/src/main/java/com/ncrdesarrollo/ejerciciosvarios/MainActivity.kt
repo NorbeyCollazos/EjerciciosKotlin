17c75984b12434aef8dab8258ca957089142a9d4
Norbey <ncrcollazos@gmail.com>
2022-01-28 14:04:30 -0500

    se configura el splashscreen

--- app/src/main/java/com/ncrdesarrollo/ejerciciosvarios/MainActivity.kt
+++ app/src/main/java/com/ncrdesarrollo/ejerciciosvarios/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Thread.sleep(2000) //esto sirve para retardar
+        setTheme(R.style.Theme_EjerciciosVarios)//se debe colocar para que tome el theme principal
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
