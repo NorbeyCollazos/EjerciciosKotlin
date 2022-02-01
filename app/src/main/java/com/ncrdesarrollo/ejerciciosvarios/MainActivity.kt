9ebf504814bf335e4e82d563a39e76447ac2d5d1
Norbey <ncrcollazos@gmail.com>
2022-02-01 17:26:49 -0500

    permisos de camara

--- app/src/main/java/com/ncrdesarrollo/ejerciciosvarios/MainActivity.kt
+++ app/src/main/java/com/ncrdesarrollo/ejerciciosvarios/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.ncrdesarrollo.ejerciciosvarios
 
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.ncrdesarrollo.ejerciciosvarios.adapter.SuperHeroAdapter
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 import com.ncrdesarrollo.ejerciciosvarios.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val CAMERA_REQUEST_CODE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //Thread.sleep(2000) //esto sirve para retardar
@@ -20,16 +25,68 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initRecyclerView()
+        initUI()
 
     }
 
-    private fun initRecyclerView() {
-        binding.recyclerview.layoutManager = LinearLayoutManager(this)
-        binding.recyclerview.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList){onItemSelected(it)}
+    private fun initUI() {
+        btnPermisosCamera.setOnClickListener {
+            chekCameraPermission()
+        }
     }
 
-    fun onItemSelected(superHero: SuperHero){
-        Toast.makeText(this, superHero.superHero, Toast.LENGTH_LONG).show()
+    private fun chekCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            //El permiso no esta aceptado
+            requestCameraPermission()
+        }else{
+            //se acepto el permiso
+            //aqui ya se puede abrir la camara
+            abrirCamara()
+        }
     }
+
+    private fun abrirCamara() {
+        Toast.makeText(this, "Abriendo camara", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            //El usuario ya ha rechazado el permiso anteriormente, debemos informarle que vaya a ajustes.
+            Toast.makeText(this, "Rechazaste los permisos, ve a austes y activlos manualmente", Toast.LENGTH_SHORT).show()
+        } else {
+            //El usuario nunca ha aceptado ni rechazado, así que le pedimos que acepte el permiso.
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //El usuario ha aceptado el permiso, no tiene porqué darle de nuevo al botón, podemos lanzar
+                // la funcionalidad desde aquí.
+                    abrirCamara()
+
+                } else {
+                    //El usuario ha rechazado el permiso, podemos desactivar la funcionalidad o mostrar una vista/diálogo.
+                    Toast.makeText(this, "Se rechazó el permiso", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+            else -> {
+                // Este else lo dejamos por si sale un permiso que no teníamos controlado.
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/ncrdesarrollo/ejerciciosvarios/SuperHero.kt
+++
@@ -1,8 +0,0 @@
-package com.ncrdesarrollo.ejerciciosvarios
-
-data class SuperHero(
-    val superHero: String,
-    val publisher: String,
-    val realName: String,
-    val photo: String,
-    )
