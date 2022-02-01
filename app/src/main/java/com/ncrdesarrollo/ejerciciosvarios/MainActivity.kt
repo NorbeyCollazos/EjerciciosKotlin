72a032fc50c3b5f4bb8d8e67a2d44dac8063ab2c
Norbey <ncrcollazos@gmail.com>
2022-02-01 13:07:24 -0500

    creando recyclerview

--- app/src/main/java/com/ncrdesarrollo/ejerciciosvarios/MainActivity.kt
+++ app/src/main/java/com/ncrdesarrollo/ejerciciosvarios/MainActivity.kt
@@ -3,14 +3,33 @@ package com.ncrdesarrollo.ejerciciosvarios
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ncrdesarrollo.ejerciciosvarios.adapter.SuperHeroAdapter
+import com.ncrdesarrollo.ejerciciosvarios.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //Thread.sleep(2000) //esto sirve para retardar
         setTheme(R.style.Theme_EjerciciosVarios)//se debe colocar para que tome el theme principal
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        initRecyclerView()
 
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerview.layoutManager = LinearLayoutManager(this)
+        binding.recyclerview.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList){onItemSelected(it)}
+    }
+
+    fun onItemSelected(superHero: SuperHero){
+        Toast.makeText(this, superHero.superHero, Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/ncrdesarrollo/ejerciciosvarios/SuperHero.kt
@@ -0,0 +1,8 @@
+package com.ncrdesarrollo.ejerciciosvarios
+
+data class SuperHero(
+    val superHero: String,
+    val publisher: String,
+    val realName: String,
+    val photo: String,
+    )
